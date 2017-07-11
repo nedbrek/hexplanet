02a1e0ca892ce5dd0dd770445f8183160b8fc73d
Edward Brekelbaum <nedbrek@yahoo.com>
2017-07-10 21:43:30 -0500

    Implemented terrain/plate view switching

--- glfwViewer/glfwViewer.cpp
+++ glfwViewer/glfwViewer.cpp
@@ -51,30 +51,32 @@ int main(int argc, char **argv)
 {
 	initGraphics();
 
-	const GLuint prgId = makeShaderProgram("vert.glsl", "frag.glsl");
-	if (!prgId)
+	const GLuint prgIdT = makeShaderProgram("vert.glsl", "frag.glsl");
+	if (!prgIdT)
 	{
-		std::cout << "Failed to load shaders." << std::endl;
+		std::cout << "Failed to load main shaders." << std::endl;
 		return 1;
 	}
+
+	const GLuint prgIdP = makeShaderProgram("vert.glsl", "plateFrag.glsl");
+	if (!prgIdP)
+	{
+		std::cout << "Failed to load plate shaders." << std::endl;
+		return 1;
+	}
+
+	// our arrays are:
+	// 0 - position
+	// 1 - terrain data
+	// 2 - plate data
+
+	// default binding is 1, change to 2
+	glBindAttribLocation(prgIdP, 2, "terrainData");
+	glLinkProgram(prgIdP); // relink after changing binding
 
 	HexPlanet p;
 	std::ifstream is("../test/sphere7.fixed.obj");
 	p.read(is);
-
-	GLuint vao;
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-
-	glUseProgram(prgId);
-
-	GLuint positionVbo;
-	glGenBuffers(1, &positionVbo);
-	glBindBuffer(GL_ARRAY_BUFFER, positionVbo);
-	glBufferData(GL_ARRAY_BUFFER, p.numTriangles()*3*3*4, NULL, GL_STATIC_DRAW);
-	void *positionBufferV = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-	float *positionBuffer = static_cast<float*>(positionBufferV);
-	assert(positionBuffer);
 
 	FILE *terrainFile = fopen("../test/terrain7.bin", "rb");
 	MapData<uint8_t> terrainFileData;
@@ -84,7 +86,31 @@ int main(int argc, char **argv)
 		return 1;
 	}
 
+	FILE *plateFile = fopen("../test/plate7.bin", "rb");
+	MapData<uint8_t> plateFileData;
+	if (!plateFile || plateFileData.read(plateFile))
+	{
+		printf("Failed to read plate data\n");
+		return 1;
+	}
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	glUseProgram(prgIdT);
+
+	GLuint positionVbo;
+	glGenBuffers(1, &positionVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, positionVbo);
+	glBufferData(GL_ARRAY_BUFFER, p.numTriangles()*3*3*4, NULL, GL_STATIC_DRAW);
+	void *positionBufferV = glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+	float *positionBuffer = static_cast<float*>(positionBufferV);
+	assert(positionBuffer);
+
 	std::vector<uint8_t> terrainData;
+	std::vector<uint8_t> plateData;
+	std::vector<float> positionData;
 	for (size_t i = 0; i < p.numTriangles(); ++i)
 	{
 		const HexTri &t = p.triangle(i);
@@ -103,6 +129,18 @@ int main(int argc, char **argv)
 		*positionBuffer = p.hex(t.m_hexC).m_vertPos.y; ++positionBuffer;
 		*positionBuffer = p.hex(t.m_hexC).m_vertPos.z; ++positionBuffer;
 
+		plateData.push_back(plateFileData[t.m_hexA]);
+		plateData.push_back(plateFileData[t.m_hexB]);
+		plateData.push_back(plateFileData[t.m_hexC]);
+
+		plateData.push_back(plateFileData[t.m_hexA]);
+		plateData.push_back(plateFileData[t.m_hexB]);
+		plateData.push_back(plateFileData[t.m_hexC]);
+
+		plateData.push_back(plateFileData[t.m_hexA]);
+		plateData.push_back(plateFileData[t.m_hexB]);
+		plateData.push_back(plateFileData[t.m_hexC]);
+
 		terrainData.push_back(terrainFileData[t.m_hexA]);
 		terrainData.push_back(terrainFileData[t.m_hexB]);
 		terrainData.push_back(terrainFileData[t.m_hexC]);
@@ -117,18 +155,28 @@ int main(int argc, char **argv)
 	}
 	glUnmapBuffer(GL_ARRAY_BUFFER);
 
-	const GLint posAttrib = glGetAttribLocation(prgId, "position");
+	const GLint posAttrib = glGetAttribLocation(prgIdT, "position");
 	glVertexAttribPointer(posAttrib, 3, GL_FLOAT, GL_FALSE, 0, 0);
 	glEnableVertexAttribArray(posAttrib);
 
-	GLuint terrainVbo;
-	glGenBuffers(1, &terrainVbo);
-	glBindBuffer(GL_ARRAY_BUFFER, terrainVbo);
+	GLuint terrainTVbo;
+	glGenBuffers(1, &terrainTVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, terrainTVbo);
 	glBufferData(GL_ARRAY_BUFFER, terrainData.size(), &terrainData[0], GL_STATIC_DRAW);
 
-	const GLint tdAttrib = glGetAttribLocation(prgId, "terrainData");
-	glVertexAttribIPointer(tdAttrib, 3, GL_UNSIGNED_BYTE, 0, 0);
-	glEnableVertexAttribArray(tdAttrib);
+	const GLint tdTAttrib = glGetAttribLocation(prgIdT, "terrainData");
+	glVertexAttribIPointer(tdTAttrib, 3, GL_UNSIGNED_BYTE, 0, 0);
+
+	// plate
+	glUseProgram(prgIdP);
+
+	GLuint terrainPVbo;
+	glGenBuffers(1, &terrainPVbo);
+	glBindBuffer(GL_ARRAY_BUFFER, terrainPVbo);
+	glBufferData(GL_ARRAY_BUFFER, plateData.size(), &plateData[0], GL_STATIC_DRAW);
+
+	const GLint tdPAttrib = glGetAttribLocation(prgIdP, "terrainData");
+	glVertexAttribIPointer(tdPAttrib, 3, GL_UNSIGNED_BYTE, 0, 0);
 
 	Camera camera;
 
@@ -137,13 +185,32 @@ int main(int argc, char **argv)
 
 	Controls ctl;
 
+	bool showTerrain = true;
 	bool running = true;
 	while (running)
 	{
+		if (showTerrain)
+		{
+			glDisableVertexAttribArray(tdPAttrib);
+			glUseProgram(prgIdT);
+			glEnableVertexAttribArray(tdTAttrib);
+		}
+		else
+		{
+			glDisableVertexAttribArray(tdTAttrib);
+			glUseProgram(prgIdP);
+			glEnableVertexAttribArray(tdPAttrib);
+		}
+
 		ctl.beginFrame(&camera);
 
 		glDrawArrays(GL_TRIANGLES, 0, p.numTriangles()*3);
+
 		glfwSwapBuffers();
+
+		if (glfwGetKey('P'))
+			showTerrain = !showTerrain;
+
 		running = !glfwGetKey(GLFW_KEY_ESC) && glfwGetWindowParam(GLFW_OPENED);
 	}
 
